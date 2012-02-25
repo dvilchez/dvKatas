22a9281c7394913676de0a4bb0325b60fba1d59a
dvilchez <dvilchezm@gmail.com>
2012-02-25 14:22:44 +0100

    sudoku v2

--- sudoku/gen/com/xuaps/android/sudoku/R.java
+++ sudoku/gen/com/xuaps/android/sudoku/R.java
@@ -30,16 +30,27 @@ public final class R {
         public static final int puzzle_selected=0x7f060009;
     }
     public static final class id {
-        public static final int about_button=0x7f090003;
+        public static final int about_button=0x7f09000d;
         public static final int about_content=0x7f090000;
-        public static final int continue_button=0x7f090001;
-        public static final int exit_button=0x7f090004;
-        public static final int new_button=0x7f090002;
-        public static final int settings=0x7f090005;
+        public static final int continue_button=0x7f09000b;
+        public static final int exit_button=0x7f09000e;
+        public static final int keypad=0x7f090001;
+        public static final int keypad_1=0x7f090002;
+        public static final int keypad_2=0x7f090003;
+        public static final int keypad_3=0x7f090004;
+        public static final int keypad_4=0x7f090005;
+        public static final int keypad_5=0x7f090006;
+        public static final int keypad_6=0x7f090007;
+        public static final int keypad_7=0x7f090008;
+        public static final int keypad_8=0x7f090009;
+        public static final int keypad_9=0x7f09000a;
+        public static final int new_button=0x7f09000c;
+        public static final int settings=0x7f09000f;
     }
     public static final class layout {
         public static final int about=0x7f020000;
-        public static final int main=0x7f020001;
+        public static final int keypad=0x7f020001;
+        public static final int main=0x7f020002;
     }
     public static final class menu {
         public static final int menu=0x7f080000;
