ee8c0fced8886c812948eabb0a6a8e09abb1836f
dvilchez <dvilchezm@gmail.com>
2012-02-27 21:59:18 +0100

    apache license

--- sudoku/application/src/main/java/com/xuaps/android/sudoku/Prefs.java
+++ sudoku/application/src/main/java/com/xuaps/android/sudoku/Prefs.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 /**
- * Created by IntelliJ IDEA.
- * User: dvilchez
- * Date: 2/19/12
- * Time: 5:32 PM
- * To change this template use File | Settings | File Templates.
- */
+*   Copyright 2012 David Vílchez Mendoza
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
 public class Prefs extends PreferenceActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
