7942e78360f5b206175a265ea2c9ff9888d5c5ce
dvilchez <dvilchezm@gmail.com>
2012-06-11 22:23:27 +0200

    code kata en vigo

--- CodeKataStringCalculatorRuby/lib/stringcalculator/calculator.rb
+++ CodeKataStringCalculatorRuby/lib/stringcalculator/calculator.rb
@@ -1,4 +1,10 @@
 module StringCalculator
 	class Calculator
+		def self.Add(numbers)
+			sum=0
+			numbers.split(%r{[,\n]{1,}}).each {|x| sum+=x.to_i}
+			
+			return sum
+		end
 	end
 end
--- CodeKataStringCalculatorRuby/spec/stringcalculator/calculator_spec.rb
+++ CodeKataStringCalculatorRuby/spec/stringcalculator/calculator_spec.rb
@@ -1,7 +1,43 @@
 require File.join(File.dirname(__FILE__), "/../spec_helper")
 
 module StringCalculator
-	describe Calculator do
-		
+	describe "Calculator" do
+		describe "Add" do
+			context "cuando cadena vacia" do
+				it "la suma debería ser 0" do
+					Calculator.Add("").should==0
+				end
+			end
+
+			context "cuando un número" do
+				it "la suma debería ser el número" do
+					Calculator.Add("1").should==1
+				end
+			end 
+
+			context "cuando dos números separados por comas" do
+				it "la suma debería ser la suma de los dos" do
+					Calculator.Add("1,2").should==3
+				end
+			end
+
+			context "cuando recibe una cantidad desconocida de números separados por comas" do
+				it "la suma debería ser la suma de todos los números de la cadena" do
+					Calculator.Add("1,2,3").should==6
+				end
+			end
+
+			context "cuando se recibe números separados por comas y/o retorno de carro" do
+				it "la suma debería ser la suma de los números de la cadena" do
+					Calculator.Add("1\n2,3").should==6
+				end
+			end
+
+			context "cuando se recibe //[separador]\n[número][separador][número]..." do
+				it "la suma debería ser la suma de los números de la cadena" do
+					Calculator.Add("//;\n1;3;3").should==7
+				end
+			end
+		end
 	end
 end
