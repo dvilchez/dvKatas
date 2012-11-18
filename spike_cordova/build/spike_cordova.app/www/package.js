44b72fcdb93f208e1881de00d134bc27ec8c7374
David Vílchez Mendoza <dvilchez@Davids-MacBook-Pro.local>
2012-11-18 18:24:19 +0100

    cleaaning the house

--- spike_cordova/build/spike_cordova.app/www/package.js
+++
@@ -1,7 +0,0 @@
-enyo.depends(
-	  "$lib/onyx",
-	  "$lib/layout/fittable",
-  "Tweet.js",
-  "TweetApp.js",
-  "TwitterSearchApp.js"
-);
--- spike_cordova/build/spike_cordova.build/Debug-iphonesimulator/spike_cordova.build/Objects-normal/i386/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/dvilchez/Documents/desarrollo_limpio/mis_proyectos/dvKatas/spike_cordova/spike_cordova/main.m
